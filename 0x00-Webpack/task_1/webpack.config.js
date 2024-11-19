ee543beb561b88c2a27d5b53eac70ac4cf5ab4fc
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-11-19 09:30:52 +0100

    Learning how to use Webpack with a config file

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/dashboard_main.js',
+  entry: './js/dashboard_main.js', // Entry point for Webpack
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'), // Output directory
+    filename: 'bundle.js', // Output file name
   },
-  mode: 'production',
+  mode: 'production', // Set Webpack mode to production
 };
